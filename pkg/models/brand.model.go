b148d72bf472bae1e08c81ccd94b1f0f88ebee04
Alvaro Gonzalez <agonzalez@minsa.gob.ni>
2025-03-18 11:13:44 -0600

    :zap: actualizacion de campos de item_brand model

--- pkg/models/brand.model.go
+++ pkg/models/brand.model.go
@@ -5,6 +5,7 @@ import "github.com/google/uuid"
 type ItemBrandModel struct {
 	Id        uuid.UUID          `gorm:"primaryKey;column:id;type:varchar(36);unique"`
 	BrandName string             `gorm:"column:brand_name;type:varchar(100);not null;unique"`
+	IsActive  bool               `gorm:"column:is_active;type:bool;not null;default:true"`
 	CreatedAt int64              `gorm:"column:created_at;type:bigint;not null;"`
 	UpdatedAt int64              `gorm:"column:updated_at;type:bigint;null;default:0"`
 	Items     []CatalogItemModel `gorm:"foreignKey:brand_id"`
