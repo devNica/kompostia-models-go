224ae79c21fc0243a8b8d0556d79c7c3614f6d88
Alvaro Gonzalez <agonzalez@minsa.gob.ni>
2025-03-19 14:55:30 -0600

    :ambulance: :bug: correccion de consulta de categorias registradas

--- pkg/models/queries/registered_categories.sql
+++ pkg/models/queries/registered_categories.sql
@@ -1,10 +1,10 @@
 SELECT c1.id,
-       c1.name AS categoryName,
+       c1.name AS "categoryName",
        c1.nomeclature,
-       c1.parent_id AS parentId,
-       c1.multi_lang_values AS multiLangCategory,
-       c1.multi_lang_is_active AS multiLangIsActive,
-       c2.name as parentName
+       c1.parent_id AS "parentId",
+       c1.multi_lang_values AS "multiLangCategory",
+       c1.multi_lang_is_active AS "multiLangIsActive",
+       c2.name AS "parentName"
 FROM {{schema}}.category c1
 LEFT JOIN {{schema}}.category c2 ON c2.id = c1.parent_id
 WHERE 
