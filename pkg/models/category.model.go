b4910f6d896fe627c24557e57993eb45f1e0e1a2
Alvaro Gonzalez <agonzalez@minsa.gob.ni>
2025-03-19 11:08:40 -0600

    :recycle: nueva correcion de tipos en definicion de modelos que ocupan multilangvalues

--- pkg/models/category.model.go
+++ pkg/models/category.model.go
@@ -3,21 +3,21 @@ package models
 import (
 	"database/sql"
 
+	"github.com/devnica/kompostia-models-go/pkg/interfaces"
 	"github.com/google/uuid"
-	"github.com/jackc/pgtype"
 )
 
 type CategoryModel struct {
-	Id                uuid.UUID          `gorm:"primaryKey;column:id;type:uuid;unique;default:gen_random_uuid()"`
-	CategoryName      string             `gorm:"column:category_name;type:varchar(100);not null;unique"`
-	Nomeclature       string             `gorm:"column:nomeclature;type:varchar(10);not null;unique"`
-	ParentId          sql.NullString     `gorm:"column:parent_id;type:uuid;null"`
-	MultiLangValues   pgtype.JSONB       `gorm:"column:multi_lang_values;type:jsonb;default:'[]'"`
-	MultiLangIsActive bool               `gorm:"column:multi_lang_is_active;type:bool;not null;default:false"`
-	IsActive          bool               `gorm:"column:is_active;type:bool;not null;default:true"`
-	CreatedAt         int64              `gorm:"column:created_at;type:bigint;not null;"`
-	UpdatedAt         int64              `gorm:"column:updated_at;type:bigint;null;default:0"`
-	Items             []CatalogItemModel `gorm:"foreignKey:category_id"`
+	Id                uuid.UUID                   `gorm:"primaryKey;column:id;type:uuid;unique;default:gen_random_uuid()"`
+	CategoryName      string                      `gorm:"column:category_name;type:varchar(100);not null;unique"`
+	Nomeclature       string                      `gorm:"column:nomeclature;type:varchar(10);not null;unique"`
+	ParentId          sql.NullString              `gorm:"column:parent_id;type:uuid;null"`
+	MultiLangValues   []interfaces.MultiLangModel `gorm:"column:multi_lang_values;type:jsonb;default:'[]'"`
+	MultiLangIsActive bool                        `gorm:"column:multi_lang_is_active;type:bool;not null;default:false"`
+	IsActive          bool                        `gorm:"column:is_active;type:bool;not null;default:true"`
+	CreatedAt         int64                       `gorm:"column:created_at;type:bigint;not null;"`
+	UpdatedAt         int64                       `gorm:"column:updated_at;type:bigint;null;default:0"`
+	Items             []CatalogItemModel          `gorm:"foreignKey:category_id"`
 }
 
 // Establecer nombre personalizado de la tabla
--- pkg/models/item.model.go
+++ pkg/models/item.model.go
@@ -1,28 +1,28 @@
 package models
 
 import (
+	"github.com/devnica/kompostia-models-go/pkg/interfaces"
 	"github.com/google/uuid"
-	"github.com/jackc/pgtype"
 )
 
 type CatalogItemModel struct {
-	Id                uuid.UUID              `gorm:"primaryKey;column:id;type:uuid;unique;default:gen_random_uuid()"`
-	ItemName          string                 `gorm:"column:item_name;type:varchar(255);not null;unique"`
-	Description       string                 `gorm:"column:description;type:varchar(255);not null"`
-	Reference         string                 `gorm:"column:reference;type:varchar(255);not null;unique"`
-	Sku               string                 `gorm:"column:sku;type:varchar(20);not null;unique"`
-	SupplierItemName  string                 `gorm:"column:supplier_item_name;type:varchar(255);not null;unique"`
-	SupplierItemCode  string                 `gorm:"column:supplier_item_code;type:varchar(50);not null;unique"`
-	MultiLangValues   pgtype.JSONB           `gorm:"column:multi_lang_values;type:jsonb;default:'[]'"`
-	MultiLangIsActive bool                   `gorm:"column:multi_lang_is_active;type:bool;not null;default:false"`
-	BrandId           uuid.UUID              `gorm:"column:brand_id;type;primaryKey"`
-	CategoryId        uuid.UUID              `gorm:"column:category_id;type;primaryKey"`
-	SupplierId        uuid.UUID              `gorm:"column:supplier_id;type;primaryKey"`
-	IsActive          bool                   `gorm:"column:is_active;type:bool;not null;default:true"`
-	CreatedAt         int64                  `gorm:"column:created_at;type:bigint;not null;"`
-	UpdatedAt         int64                  `gorm:"column:updated_at;type:bigint;null;default:0"`
-	Locations         []ItemHasLocationModel `gorm:"foreignKey:item_id"`
-	Files             []ItemHasFileModel     `gorm:"foreignKey:item_id"`
+	Id                uuid.UUID                   `gorm:"primaryKey;column:id;type:uuid;unique;default:gen_random_uuid()"`
+	ItemName          string                      `gorm:"column:item_name;type:varchar(255);not null;unique"`
+	Description       string                      `gorm:"column:description;type:varchar(255);not null"`
+	Reference         string                      `gorm:"column:reference;type:varchar(255);not null;unique"`
+	Sku               string                      `gorm:"column:sku;type:varchar(20);not null;unique"`
+	SupplierItemName  string                      `gorm:"column:supplier_item_name;type:varchar(255);not null;unique"`
+	SupplierItemCode  string                      `gorm:"column:supplier_item_code;type:varchar(50);not null;unique"`
+	MultiLangValues   []interfaces.MultiLangModel `gorm:"column:multi_lang_values;type:jsonb;default:'[]'"`
+	MultiLangIsActive bool                        `gorm:"column:multi_lang_is_active;type:bool;not null;default:false"`
+	BrandId           uuid.UUID                   `gorm:"column:brand_id;type;primaryKey"`
+	CategoryId        uuid.UUID                   `gorm:"column:category_id;type;primaryKey"`
+	SupplierId        uuid.UUID                   `gorm:"column:supplier_id;type;primaryKey"`
+	IsActive          bool                        `gorm:"column:is_active;type:bool;not null;default:true"`
+	CreatedAt         int64                       `gorm:"column:created_at;type:bigint;not null;"`
+	UpdatedAt         int64                       `gorm:"column:updated_at;type:bigint;null;default:0"`
+	Locations         []ItemHasLocationModel      `gorm:"foreignKey:item_id"`
+	Files             []ItemHasFileModel          `gorm:"foreignKey:item_id"`
 }
 
 // Establecer nombre personalizado de la tabla
