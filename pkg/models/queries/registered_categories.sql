8db8c4d2f142e8f51e73bec9d39c8bcda9095774
Alvaro Gonzalez <agonzalez@minsa.gob.ni>
2025-03-19 15:52:00 -0600

    :ambulance:

--- pkg/models/queries/registered_categories.sql
+++ pkg/models/queries/registered_categories.sql
@@ -2,7 +2,7 @@ SELECT c1.id "id",
        c1.category_name "categoryName",
        c1.nomeclature "nomeclature",
        c1.parent_id "parentId",
-       c1.multi_lang_values "multiLangCategory",
+       COALESCE(c1.multi_lang_values, '[]') "multiLangCategory",
        c1.multi_lang_is_active "multiLangIsActive",
        c2.category_name "parentName"
 FROM {{schema}}.category c1
