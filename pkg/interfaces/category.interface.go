e3fbb61c510dd27b77acd96aba59ec38fcc3261a
Alvaro Gonzalez <agonzalez@minsa.gob.ni>
2025-03-19 16:07:36 -0600

    :zap:

--- pkg/interfaces/category.interface.go
+++ pkg/interfaces/category.interface.go
@@ -20,11 +20,11 @@ type FlatCategoryI struct {
 
 // Estructura recomendada para listar todas las categorias
 type ReflexCategoryI struct {
-	Id                string           `json:"id"`
-	CategoryName      string           `json:"categoryName"`
-	Nomeclature       string           `json:"nomeclature"`
-	ParentID          *string          `json:"parentId"`
-	MultiLangCategory []MultiLangModel `json:"multiLangCategory"`
-	MultiLangIsActive bool             `json:"multiLangIsActive"`
-	ParentName        *string          `json:"parentName"`
+	Id                string  `json:"id"`
+	CategoryName      string  `json:"categoryName"`
+	Nomeclature       string  `json:"nomeclature"`
+	ParentID          *string `json:"parentId"`
+	MultiLangCategory string  `json:"multiLangCategory"`
+	MultiLangIsActive bool    `json:"multiLangIsActive"`
+	ParentName        *string `json:"parentName"`
 }
