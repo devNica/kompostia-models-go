6e6049dd8201d78d365738ec96379222e525c731
Alvaro Gonzalez <agonzalez@minsa.gob.ni>
2025-03-19 15:29:02 -0600

    :ambulance: prueba #1 de cambios realizados a la interfaz reflex_category

--- pkg/models/queries/registered_categories.sql
+++ pkg/models/queries/registered_categories.sql
@@ -1,10 +1,10 @@
-SELECT c1.id,
-       c1.category_name AS "categoryName",
-       c1.nomeclature,
-       c1.parent_id AS "parentId",
-       c1.multi_lang_values AS "multiLangCategory",
-       c1.multi_lang_is_active AS "multiLangIsActive",
-       c2.category_name AS "parentName"
+SELECT c1.id "id",
+       c1.category_name "categoryName",
+       c1.nomeclature "nomeclature",
+       c1.parent_id "parentId",
+       c1.multi_lang_values "multiLangCategory",
+       c1.multi_lang_is_active "multiLangIsActive",
+       c2.category_name "parentName"
 FROM {{schema}}.category c1
 LEFT JOIN {{schema}}.category c2 ON c2.id = c1.parent_id
 WHERE 
